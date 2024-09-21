1bc5325e6ede9da00e630cd0553fd8228e1de75e
İlayda Özel <53452296+ilaydaozel@users.noreply.github.com>
2024-09-21 18:35:51 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,37 @@
+const USERS_ENDPOINT = 'https://jsonplaceholder.typicode.com/users'; 
+//////////////////////////////////////////////////////////////////// 
+
+function renderColumn(title, users) { 
+    // Create column
+    const columnDiv = document.createElement('div'); 
+    columnDiv.classList.add('column'); 
+    // Create column title
+    const h3 = document.createElement('h3'); 
+    h3.textContent = title; 
+    columnDiv.appendChild(h3); 
+
+    // Iterate through users
+    users.forEach((user) => { 
+        // Create card for a user
+        const cardDiv = document.createElement('div'); 
+        cardDiv.classList.add('card'); 
+        // Name text
+        const nameP = document.createElement('p'); 
+        nameP.textContent = `Name: ${user.name}`; 
+        cardDiv.appendChild(nameP); 
+        // Username text
+        const usernameP = document.createElement('p'); 
+        usernameP.textContent = `Username: ${user.username}`; 
+        cardDiv.appendChild(usernameP); 
+        // Website text
+        const websiteP = document.createElement('p'); 
+        websiteP.textContent = `Website: ${user.website}`; 
+        cardDiv.appendChild(websiteP); 
+        // Put the card to the column div
+        columnDiv.appendChild(cardDiv); 
+    }); 
+    // Wrap the column
+    const wrapperDiv = document.getElementById('wrapper'); 
+    wrapperDiv.appendChild(columnDiv); 
+}
+renderColumn();
